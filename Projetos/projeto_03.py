d6932be3717b4d398b57d494cbed66f9ccba0b8c
caiomanfredini <caiomanfredini.15@gmail.com>
2021-06-25 10:59:07 -0300

    Adicionando efeitos visuais Projeto 3

--- Projetos/projeto_03.py
+++ Projetos/projeto_03.py
@@ -12,26 +12,50 @@
 '''
 
 from random import choices
+from time import sleep
+from os import system
 
 # Laço de repetição para manter a execução do jogo
 while True:
+    # Limpando o terminal pra ficar bonitinho
+    system('cls')
     # Dicionário que contém a quantidade de vitórias
     # de cada jogador
     rodadas_vencidas = {1: 0, 2: 0, 3: 0, 4: 0}
-
+    # Leitura da quantidade de rodadas
     rodadas = int(input('Quantas rodadas você deseja? '))
+    # Limpando o terminal pra ficar bonitinho
+    system('cls')
     # Laço para executar em cada rodada
     for r in range(rodadas):
         # 'jogadas' contém a jogada da rodada atual de cada um dos quatro jogadores
         jogadas = choices(range(1, 7), k=4)
+        # Mostrar a jogada de cada jogador pra ficar bonitinho
+        for j in range(4):
+            print(f'\nJogador {j + 1} jogou o 🎲 e deu... {jogadas[j]}!')
+            sleep(2.5)
         # Caso apenas um jogador tenha tirado o maior número na rodada
         if jogadas.count(max(jogadas)) == 1:
             # Gravo o index do maior número
             vencedor = jogadas.index(max(jogadas)) + 1
+            # Mostrando o vencedor da rodada atual
+            print(f'\nO vencedor da rodada é o Jogador {vencedor}!')
+            sleep(2)
             # Incremendo a vitória no dicionário de rodadas_vencidas
             rodadas_vencidas[vencedor] = rodadas_vencidas[vencedor] + 1
-
+        else:
+            print(f'\nXii... Empatou! Vamos de próxima!')
+            sleep(2)
+        # Limpando o Terminal
+        system('cls')
+    # Ordenando o dicionário do jogador que teve mais vitórias
+    # para o que teve menos vitórias
+    rodadas_vencidas = sorted(rodadas_vencidas.items(),
+                              key=lambda x: x[1], reverse=True)
+    # Printa o vencedor (rodadas_vencidas[0])
+    print(
+        f'O jogador {rodadas_vencidas[0][0]} ganhou {rodadas_vencidas[0][1]} partidas! Ele venceu!')
     # Caso o jogador não queira continuar, termina o jogo
-    flag = input('Deseja continuar? [S/N] ').strip().upper()
+    flag = input('\nDeseja continuar? [S/N] ').strip().upper()
     if flag == 'N':
         break
